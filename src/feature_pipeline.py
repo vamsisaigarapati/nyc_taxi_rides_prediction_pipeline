b1b35dd490844022f79783daed74dfc3670340c9
Vamsi Sai Garapati <35292779+vamsisaigarapati@users.noreply.github.com>
2025-03-01 01:15:44 -0500

    feature_pipelineeeeee

--- src/feature_pipeline.py
+++ src/feature_pipeline.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta, timezone
 import hopsworks
 import pandas as pd
 
-import src.config as config
-from src.data_utils import fetch_batch_raw_data, transform_raw_data_into_ts_data
+import config as config
+from data_utils import fetch_batch_raw_data, transform_raw_data_into_ts_data
 
 # Configure logging
 logging.basicConfig(
